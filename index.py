2d4591ea981637770d9b98a514bb124c74ee489d
Edwin574 <owinoedwin574@gmail.com>
2023-02-13 15:18:08 +0300

    initial commit

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ df = pd.read_csv('gapminder_with_codes.csv')
 data_2007 = df[df['year'] == 2007]
 
 st.dataframe(data_2007)
+
 fig,ax=plt.subplots()
 
 sns.set_theme()
@@ -17,6 +18,6 @@ violin_data=[]
 violin_data.append(data_2007['gdpPercap'])
 violin_data.append(data_2007['pop'])
 violin_data.append(data_2007['lifeExp'])
-sns.violinplot(data=violin_data,palette='light:r',inner='box',orient='h',scale='count',gridsize=150,ax=ax)
+sns.violinplot(data=violin_data,palette='light:b',inner='box',orient='h',scale='count',gridsize=150,ax=ax)
 
 st.pyplot(fig)
